41c6daccab998ba346d24d9704c45edf7170de68
TunaLobster <charliepjohnson@gmail.com>
2021-04-05 11:43:53 -0500

    Clean up from debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 #include <Arduino.h>
 
-// #include <Servo.h>
-
-// Servo myservo;
-
 #include <Servo8Bit.h>
 
 Servo8Bit myservo;
 
 uint8_t potpin = A1;
+uint8_t servopin = 1; // PB1
 uint8_t servo_rate = 20; // millis
 
 uint8_t val;
@@ -16,26 +13,20 @@ uint8_t now;
 uint8_t old_now;
 uint8_t last_servo_out;
 
-int blah = 37483;
-
-
 void setup() {
-  myservo.attach(1); // attach to PB1
+  myservo.attach(servopin);
 }
 
 void loop() {
   now = millis();
-  if ((now >= last_servo_out + servo_rate) || (old_now > now)) // only send to servo when it is time
+  if ((now >= last_servo_out + servo_rate) || (old_now > now)) // only send to servo when it is time or if millis wraps
   {
-    blah++;
-    analogWrite(0, blah);
     // get analog and map to PWM mircos
     val = analogRead(potpin);
-    val = map(val, 0, 1023, 0, 180);
+    val = map(val, 0, 1023, 800, 2200);
 
     // set servo
-    // myservo.writeMicroseconds(val);
-    myservo.write(val);
+    myservo.writeMicroseconds(val);
     last_servo_out = now;
   }
   old_now = now;
